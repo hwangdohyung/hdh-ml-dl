4b75ef6a6460e068d2492ade9f4de3183b48819a
hwangdohyung <107665717+hwangdohyung@users.noreply.github.com>
2022-06-21 13:22:31 +0900

    6.21

--- keras/keras05_train_test2.py
+++ keras/keras05_train_test2.py
@@ -6,6 +6,16 @@ from tensorflow.keras.layers import Dense
 x = np.array([1,2,3,4,5,6,7,8,9,10])
 y = np.array([1,2,3,4,5,6,7,8,9,10])
 
+x_train = x[:7]
+x_test = x[7:]
+y_train = y[:7]
+y_test = y[7:]
+
+print(x_train)
+print(x_test)
+print(y_train)
+print(y_test)
+
 #[과제] 넘파이 리스트의 슬라이싱!! 7:3으로 자르기
 
 # x_train = np.array([1,2,3,4,5,6,7])               
