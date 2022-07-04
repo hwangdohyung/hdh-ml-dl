6027f8791c1bc30132743dd212ce0f043481f403
hwangdohyung <107665717+hwangdohyung@users.noreply.github.com>
2022-07-04 20:01:19 +0900

    7.4

--- keras/keras16_onehot.py
+++ keras/keras16_onehot.py
@@ -1,11 +1,11 @@
 #[과제]
 #3가지 onehotencoding 방식을 비교할것
 
-#1.pandas의 get_dummies - argmax 부분은 numpy말고 tensorflow로 하면 돌아간다. 라벨인코딩은 필요가없다.
+#1.pandas의 get_dummies - argmax 부분은 numpy말고 tensorflow로 하면 돌아간다. 겟더미는 라벨인코딩은 필요가없다.
 
 
 
-#2.tensorflow의 to_categorical - 컬럼 구성을 무조건 0부터 시작하게됨. 0이없으면 생성하게됨
+#2.tensorflow의 to_categorical - 컬럼 구성을 무조건 0부터 시작하게됨. 0이없어도 생성하게됨
 
 
 
