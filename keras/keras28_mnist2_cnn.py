b13358230721044c5ac05b62bb0bcd1ac7712078
hwangdohyung <107665717+hwangdohyung@users.noreply.github.com>
2022-07-11 09:15:07 +0900

    dd

--- keras/keras28_mnist2_cnn.py
+++ keras/keras28_mnist2_cnn.py
@@ -7,6 +7,7 @@ from tensorflow.python.keras.callbacks import EarlyStopping
 import tensorflow as tf
 import pandas as pd
 import numpy as np
+from sklearn.preprocessing import MaxAbsScaler,MinMaxScaler,RobustScaler,StandardScaler
 
 #1.데이터
 (x_train,y_train),(x_test,y_test) = mnist.load_data()
@@ -17,14 +18,18 @@ print(y_train.shape,y_test.shape)
 
 
 
+# one hot encoding
+from tensorflow.keras.utils import to_categorical
+y = to_categorical(y_train) 
+y = to_categorical(y_test)
 
-'''
-# # one hot encoding
-# from tensorflow.keras.utils import to_categorical
-# y = to_categorical(y_train) 
-# # 트레인 테스트 분리
-# x_train, x_test, y_train, y_test = train_test_split(x,y, test_size=0.3,shuffle=True,
-#                                                      random_state=58)
+# minmax , standard
+# scaler = MinMaxScaler()
+# # scaler = StandardScaler()
+# # scaler = MaxAbsScaler()
+# # scaler = RobustScaler()
+# x_train = scaler.fit_transform(x_train)#스케일링한것을 보여준다.
+# x_test = scaler.transform(x_test)#test는 transfrom만 해야됨 
 
 #2.모델구성
 model = Sequential()
@@ -36,8 +41,28 @@ model.add(Flatten())
 model.add(Dense(32,activation = 'relu'))
 model.add(Dense(32,activation = 'relu'))
 model.add(Dense(10,activation = 'softmax'))
-model.summary() 
+
+#3.컴파일,훈련
+model.compile(loss= 'categorical_crossentropy', optimizer ='adam', metrics='accuracy') #다중분류는 무조건 loss에 categorical_crossentropy
+ #분류모델에서 셔플 중요! ,false로 하면 순차적으로 나와서 2가 아예 안나옴.
+
+earlyStopping= EarlyStopping(monitor='val_loss',patience=10,mode='min',restore_best_weights=True,verbose=1)
+
+model.fit(x_train, y_train, epochs=100, batch_size=100,validation_split=0.2,callbacks=[earlyStopping,],verbose=1) #batch default :32
+
+
+
+#4.평가,예측
+results = model.evaluate(x_test,y_test)
+print('loss : ', results[0])
+
+y_predict = model.predict(x_test)
+y_predict = tf.argmax(y_predict,axis=1) 
+
+y_test = tf.argmax(y_test,axis=1) 
+acc = accuracy_score(y_test,y_predict)
+print('acc : ',acc)
+
 
 #만들기! acc 0.98이상
 
-'''
