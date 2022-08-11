496d83b5e6fea9106da16e5745881893c62253e5
hwangdohyung <107665717+hwangdohyung@users.noreply.github.com>
2022-08-12 00:11:12 +0900

    d

--- ml/ml36_dacon_travel.py
+++ ml/ml36_dacon_travel.py
@@ -8,8 +8,13 @@ from collections import Counter
 from tensorflow.python.keras.callbacks import EarlyStopping,ModelCheckpoint
 from sklearn.preprocessing import MinMaxScaler,StandardScaler,MaxAbsScaler,RobustScaler
 import datetime as dt
-from sklearn.model_selection import KFold,GridSearchCV
+from sklearn.experimental import enable_halving_search_cv
+from sklearn.model_selection import KFold,GridSearchCV,StratifiedKFold,HalvingRandomSearchCV
 from xgboost import XGBRegressor,XGBClassifier
+from sklearn.preprocessing import LabelEncoder
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.pipeline import Pipeline, make_pipeline 
+
 
 
 ################## 1.데이터 #######################
@@ -23,70 +28,77 @@ print(test_set.shape)   #(2933, 18)
 
 #################### 결측치 #####################
 train_set = train_set.dropna()
-# test_set = test_set.dropna()
+
+means = test_set.mean() #컬럼별 평균
+test_set = test_set.fillna(means)
+
+
+
 print(train_set.shape,test_set.shape)#(1649, 19) (2479, 18)
-##################### 맵핑 ######################
-test_set['Designation'] = test_set['Designation'].map({'Executive':0,'Manager':1,'Senior Manager':2,'AVP':3,'VP':4})
-test_set['MaritalStatus'] = test_set['MaritalStatus'].map({'Married':0,'Divorced':1,'Unmarried':2,'Single':3})
-test_set['ProductPitched'] = test_set['ProductPitched'].map({'Basic':0,'Deluxe':1,'Standard':2,'Super Deluxe':3,'King':4})
-test_set['Gender'] = test_set['Gender'].map({'Male':0,'Fe Male':1,'Female':1})
-test_set['Occupation'] = test_set['Occupation'].map({'Small Business':0,'Salaried':1,'Large Business':2,'Free Lancer':3})
-test_set['TypeofContact'] = test_set['TypeofContact'].map({'Company Invited':0,'Self Enquiry':1})
+##################### 라벨인코더 ######################
+le = LabelEncoder()
+idxarr = train_set.columns
+idxarr = np.array(idxarr)
 
-train_set['Designation'] = train_set['Designation'].map({'Executive':0,'Manager':1,'Senior Manager':2,'AVP':3,'VP':4})
-train_set['MaritalStatus'] = train_set['MaritalStatus'].map({'Married':0,'Divorced':1,'Unmarried':2,'Single':3})
-train_set['ProductPitched'] = train_set['ProductPitched'].map({'Basic':0,'Deluxe':1,'Standard':2,'Super Deluxe':3,'King':4})
-train_set['Gender'] = train_set['Gender'].map({'Male':0,'Fe Male':1,'Female':1})
-train_set['Occupation'] = train_set['Occupation'].map({'Small Business':0,'Salaried':1,'Large Business':2,'Free Lancer':3})
-train_set['TypeofContact'] = train_set['TypeofContact'].map({'Company Invited':0,'Self Enquiry':1})
-
-
-
-
-
+for i in idxarr:
+      if train_set[i].dtype == 'object':
+        train_set[i] = le.fit_transform(train_set[i])
+        test_set[i] = le.fit_transform(test_set[i])
+        
+        
+print(train_set)
+print(test_set) 
+       
+       
 # from datetime import datetime, timedelta
 # from pandas import DataFrame
 # import matplotlib.pyplot as plt 
 # import seaborn as sns
-# sns.set(font_scale= 1.2)
+# sns.set(font_scale= 1.0)
 # sns.heatmap(data=train_set.corr(), square= True, annot=True, cbar=True) 
 
 # plt.show()
 
+
 x = train_set.drop('ProdTaken',axis=1)
 y = train_set['ProdTaken']
-print(x.shape)
-print(x)
-print(y.shape)
 
-x_train,x_test,y_train,y_test = train_test_split(x,y, train_size=0.8, shuffle=True, random_state=123,stratify=y)
 
-scaler = StandardScaler()
+
+x_train,x_test,y_train,y_test = train_test_split(x,y, train_size=0.8, shuffle=True, random_state=134,stratify=y)
+
+scaler = MinMaxScaler()
 x_train = scaler.fit_transform(x_train)
 x_test = scaler.transform(x_test)
 
-n_splits = 5
-kfold = KFold(n_splits=n_splits, shuffle = True, random_state = 123)
+# parameters = {'n_estimators' : [100],'learning_rate': [0.2],'max_depth': [2],'gamma': [100],'min_child_weight': [10], 'subsample': [0.1],
+#               'colsample_bytree': [0],'colsample_bylevel': [0],'colsample_bynode': [0] ,'reg_alpha': [2],'reg_lambda':[10]
+#               }
 
-parameters = {'n_estimators' : [100],'learning_rate': [0.2],'max_depth': [2],'gamma': [100],'min_child_weight': [10], 'subsample': [0.1],
-              'colsample_bytree': [0],'colsample_bylevel': [0],'colsample_bynode': [0] ,'reg_alpha': [2],'reg_lambda':[10]
-              }
-
+parameters = [
+    {'n_estimators':[100,200]},
+    {'max_depth':[6,8,10,12]},
+    {'min_samples_leaf':[3,5,7,10]},
+    {'min_samples_split':[2,3,5,10]},
+    {'n_jobs':[-1,2,4]}]
 
 #2.모델 
-xgb = XGBClassifier(random_state = 123)
+random= RandomForestClassifier()
 
-model = GridSearchCV(xgb, parameters, cv=kfold, n_jobs=8)
+model = make_pipeline(MinMaxScaler(), HalvingRandomSearchCV(random, parameters, cv=5, n_jobs=-1, verbose=2))
 
 model.fit(x_train,y_train)
 
-print('최상의 매개변수 : ', model.best_params_)
-print('최상의 점수 : ', model.best_score_)
 
 results = model.score(x_test,y_test)
 print(results)
 
 y_submmit = model.predict(test_set)
+print(y_submmit)
+
+print(pd.value_counts(y_submmit))
+
+
 
 submission = pd.read_csv(path + 'sample_submission.csv')
 submission['ProdTaken'] = y_submmit
