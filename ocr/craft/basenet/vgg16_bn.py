f742ce1f3f9343f92825e97e99a0f8b235c15a33
hwangdohyung <107665717+hwangdohyung@users.noreply.github.com>
2022-09-29 09:21:41 +0900

    d

--- ocr/craft/basenet/vgg16_bn.py
+++ ocr/craft/basenet/vgg16_bn.py
@@ -71,3 +71,8 @@ class vgg16_bn(torch.nn.Module):
         vgg_outputs = namedtuple("VggOutputs", ['fc7', 'relu5_3', 'relu4_3', 'relu3_2', 'relu2_2'])
         out = vgg_outputs(h_fc7, h_relu5_3, h_relu4_3, h_relu3_2, h_relu2_2)
         return out
+
+
+
+
+print('이형권 바보')
