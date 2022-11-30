b2f3519b77678b11c2291d5eb943c5a832dd7b12
do <fire211@naver.com>
2022-11-30 09:42:06 +0900

    f

--- bert/bert_predict.py
+++ bert/bert_predict.py
@@ -14,8 +14,6 @@ import random
 import time
 import datetime
 
-
-
 # 데이터 
 train = pd.read_csv('d:/bertdata/ratings_train.txt', sep='\t')
 test = pd.read_csv('d:/bertdata/ratings_test.txt', sep='\t')
@@ -23,7 +21,6 @@ print(train.shape)
 print(test.shape)
 print(train.head(10))
 
-#################### 트레인 셋(전처리)####################
 # 리뷰 문장 추출
 sentences = train['document']
 sentences[:10]
@@ -53,6 +50,7 @@ input_ids = [tokenizer.convert_tokens_to_ids(x) for x in tokenized_texts]
 input_ids = pad_sequences(input_ids, maxlen=MAX_LEN, dtype="long", truncating="post", padding="post")
 
 input_ids[0]
+
 # 어텐션 마스크 초기화
 attention_masks = []
 
@@ -93,7 +91,6 @@ print(validation_masks[0])
 
 # 배치 사이즈
 batch_size = 32
-
 # 파이토치의 DataLoader로 입력, 마스크, 라벨을 묶어 데이터 설정
 # 학습시 배치 사이즈 만큼 데이터를 가져옴
 train_data = TensorDataset(train_inputs, train_masks, train_labels)
@@ -104,8 +101,6 @@ validation_data = TensorDataset(validation_inputs, validation_masks, validation_
 validation_sampler = SequentialSampler(validation_data)
 validation_dataloader = DataLoader(validation_data, sampler=validation_sampler, batch_size=batch_size)
 
-
-#################### 테스트 셋(전처리)####################
 # 리뷰 문장 추출
 sentences = test['document']
 sentences[:10]
@@ -165,33 +160,25 @@ test_data = TensorDataset(test_inputs, test_masks, test_labels)
 test_sampler = RandomSampler(test_data)
 test_dataloader = DataLoader(test_data, sampler=test_sampler, batch_size=batch_size)
 
+# GPU 디바이스 이름 구함
+device_name = tf.test.gpu_device_name()
 
-    
-USE_CUDA = torch.cuda.is_available()
-device = torch.device('cuda' if USE_CUDA else 'cpu')  # 쓸 수 있는 장치가 있으면 cuda 없으면 cpu를 쓰겟다
+# 디바이스 설정
+if torch.cuda.is_available():    
+    device = torch.device("cuda")
+    print('There are %d GPU(s) available.' % torch.cuda.device_count())
+    print('We will use the GPU:', torch.cuda.get_device_name(0))
+else:
+    device = torch.device("cpu")
+    print('No GPU available, using the CPU instead.')
 
 
-
-# 분류를 위한 BERT 모델 생성
+    # 분류를 위한 BERT 모델 생성
 model = BertForSequenceClassification.from_pretrained("bert-base-multilingual-cased", num_labels=2)
+model.cuda()
+path = 'D:\study_data\_save/'
+model.load_state_dict(torch.load(path + 'torch_bert_dict.pt'))
 
-
-# 옵티마이저 설정
-optimizer = AdamW(model.parameters(),
-                  lr = 2e-5, # 학습률
-                  eps = 1e-8 # 0으로 나누는 것을 방지하기 위한 epsilon 값
-                )
-
-# 에폭수
-epochs = 4
-
-# 총 훈련 스텝 : 배치반복 횟수 * 에폭
-total_steps = len(train_dataloader) * epochs
-
-# 처음에 학습률을 조금씩 변화시키는 스케줄러 생성
-scheduler = get_linear_schedule_with_warmup(optimizer, 
-                                            num_warmup_steps = 0,
-                                            num_training_steps = total_steps)
 
 # 정확도 계산 함수
 def flat_accuracy(preds, labels):
@@ -210,180 +197,6 @@ def format_time(elapsed):
     # hh:mm:ss으로 형태 변경
     return str(datetime.timedelta(seconds=elapsed_rounded))
 
-# 재현을 위해 랜덤시드 고정
-seed_val = 42
-random.seed(seed_val)
-np.random.seed(seed_val)
-torch.manual_seed(seed_val)
-torch.cuda.manual_seed_all(seed_val)
-
-# 그래디언트 초기화
-model.zero_grad()
-
-# 에폭만큼 반복
-for epoch_i in range(0, epochs):
-    
-    # ========================================
-    #               Training
-    # ========================================
-    
-    print("")
-    print('======== Epoch {:} / {:} ========'.format(epoch_i + 1, epochs))
-    print('Training...')
-
-    # 시작 시간 설정
-    t0 = time.time()
-
-    # 로스 초기화
-    total_loss = 0
-
-    # 훈련모드로 변경
-    model.train()
-        
-    # 데이터로더에서 배치만큼 반복하여 가져옴
-    for step, batch in enumerate(train_dataloader):
-        # 경과 정보 표시
-        if step % 500 == 0 and not step == 0:
-            elapsed = format_time(time.time() - t0)
-            print('  Batch {:>5,}  of  {:>5,}.    Elapsed: {:}.'.format(step, len(train_dataloader), elapsed))
-
-        # 배치를 GPU에 넣음
-        batch = tuple(t.to(device) for t in batch)
-        
-        # 배치에서 데이터 추출
-        b_input_ids, b_input_mask, b_labels = batch
-
-        # Forward 수행                
-        outputs = model(b_input_ids, 
-                        token_type_ids=None, 
-                        attention_mask=b_input_mask, 
-                        labels=b_labels)
-        
-        # 로스 구함
-        loss = outputs[0]
-
-        # 총 로스 계산
-        total_loss += loss.item()
-
-        # Backward 수행으로 그래디언트 계산
-        loss.backward()
-
-        # 그래디언트 클리핑
-        torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
-
-        # 그래디언트를 통해 가중치 파라미터 업데이트
-        optimizer.step()
-
-        # 스케줄러로 학습률 감소
-        scheduler.step()
-
-        # 그래디언트 초기화
-        model.zero_grad()
-
-    # 평균 로스 계산
-    avg_train_loss = total_loss / len(train_dataloader)            
-
-    print("")
-    print("  Average training loss: {0:.2f}".format(avg_train_loss))
-    print("  Training epcoh took: {:}".format(format_time(time.time() - t0)))
-        
-    # ========================================
-    #               Validation
-    # ========================================
-
-    print("")
-    print("Running Validation...")
-
-    #시작 시간 설정
-    t0 = time.time()
-
-    # 평가모드로 변경
-    model.eval()
-
-    # 변수 초기화
-    eval_loss, eval_accuracy = 0, 0
-    nb_eval_steps, nb_eval_examples = 0, 0
-
-    # 데이터로더에서 배치만큼 반복하여 가져옴
-    for batch in validation_dataloader:
-        # 배치를 GPU에 넣음
-        batch = tuple(t.to(device) for t in batch)
-        
-        # 배치에서 데이터 추출
-        b_input_ids, b_input_mask, b_labels = batch
-        
-        # 그래디언트 계산 안함
-        with torch.no_grad():     
-            # Forward 수행
-            outputs = model(b_input_ids, 
-                            token_type_ids=None, 
-                            attention_mask=b_input_mask)
-        
-        # 출력 로짓 구함
-        logits = outputs[0]
-
-        # CPU로 데이터 이동
-        logits = logits.detach().cpu().numpy()
-        label_ids = b_labels.to('cpu').numpy()
-        
-        # 출력 로짓과 라벨을 비교하여 정확도 계산
-        tmp_eval_accuracy = flat_accuracy(logits, label_ids)
-        eval_accuracy += tmp_eval_accuracy
-        nb_eval_steps += 1
-
-    print("  Accuracy: {0:.2f}".format(eval_accuracy/nb_eval_steps))
-    print("  Validation took: {:}".format(format_time(time.time() - t0)))
-
-print("")
-print("Training complete!")
-
-############### 테스트셋 평가 ##################
-
-#시작 시간 설정
-t0 = time.time()
-
-# 평가모드로 변경
-model.eval()
-
-# 변수 초기화
-eval_loss, eval_accuracy = 0, 0
-nb_eval_steps, nb_eval_examples = 0, 0
-
-# 데이터로더에서 배치만큼 반복하여 가져옴
-for step, batch in enumerate(test_dataloader):
-    # 경과 정보 표시
-    if step % 100 == 0 and not step == 0:
-        elapsed = format_time(time.time() - t0)
-        print('  Batch {:>5,}  of  {:>5,}.    Elapsed: {:}.'.format(step, len(test_dataloader), elapsed))
-
-    # 배치를 GPU에 넣음
-    batch = tuple(t.to(device) for t in batch)
-    
-    # 배치에서 데이터 추출
-    b_input_ids, b_input_mask, b_labels = batch
-    
-    # 그래디언트 계산 안함
-    with torch.no_grad():     
-        # Forward 수행
-        outputs = model(b_input_ids, 
-                        token_type_ids=None, 
-                        attention_mask=b_input_mask)
-    
-    # 출력 로짓 구함
-    logits = outputs[0]
-
-    # CPU로 데이터 이동
-    logits = logits.detach().cpu().numpy()
-    label_ids = b_labels.to('cpu').numpy()
-    
-    # 출력 로짓과 라벨을 비교하여 정확도 계산
-    tmp_eval_accuracy = flat_accuracy(logits, label_ids)
-    eval_accuracy += tmp_eval_accuracy
-    nb_eval_steps += 1
-
-print("")
-print("Accuracy: {0:.2f}".format(eval_accuracy/nb_eval_steps))
-print("Test took: {:}".format(format_time(time.time() - t0)))
 
 # 입력 데이터 변환
 def convert_input_data(sentences):
@@ -452,4 +265,23 @@ logits = test_sentences(['주연배우가 아깝다. 총체적 난국...'])
 
 print(logits)
 print(np.argmax(logits))
+
+logits = test_sentences(['이형권은 너무 잘생겼다.'])
+
+print(logits)
+print(np.argmax(logits))
+
+logits = test_sentences(['이형권은 너무 못생겼다.'])
+
+print(logits)
+print(np.argmax(logits))
+
+logits = test_sentences(['이형권'])
+
+print(logits)
+print(np.argmax(logits))
+
+
+
+
 
